048cdb2684f4c82f2a33b8a21dd69f27459a8c56
osman-butt <butt.osman@gmail.com>
2023-09-26 23:12:50 +0200

    fetching results and members data

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 import { initTabs } from "./tabs.js";
 import * as member from "./member.js";
 import * as result from "./result.js";
-import { fetchResults, fetchMembers } from "rest-services.js";
+import { fetchResults, fetchMembers } from "./data-fetch.js";
 
 window.addEventListener("load", initApp);
 
+const resultsArr = [];
+const membersArr = [];
+
 async function initApp() {
   initTabs();
+  await buildResultsList();
+  await buildMembersList();
+  console.log(resultsArr);
+  console.log(membersArr);
   // TODO: Make the rest of the program ...
+}
+
+async function buildResultsList() {
+  const originalResults = await fetchResults();
+  for (const jsonObj of originalResults) {
+    const realObject = result.construct(jsonObj);
+    resultsArr.push(realObject);
+  }
+}
+
+async function buildMembersList() {
+  const originalResults = await fetchMembers();
+  for (const jsonObj of originalResults) {
+    const realObject = member.construct(jsonObj);
+    membersArr.push(realObject);
+  }
 }
